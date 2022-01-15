fc134ce5b580cc1347af634e446fdbe4326c87ee
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2022-01-15 12:59:31 +0100

    Add splash screen

---
+++ app/src/main/java/tech/szymanska/mypocketdoctor/SplashScreen.java
@@ -0,0 +1,23 @@
+package tech.szymanska.mypocketdoctor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timer = new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException iEx) {
+                iEx.printStackTrace();
+            }
+        });
+        timer.start();
+    }
+}
